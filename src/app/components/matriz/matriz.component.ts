1c88dd45dd24bf578c587b7d32aea96c7f5ad961
julio-1610 <jpaucarc@unsa.edu.pe>
2025-01-09 16:07:17 -0500

    Cambio de tipo de variable de matrizRotada para que almacene una matriz 2x2, agregación de validaciones para obtener una matriz 2x2

--- src/app/components/matriz/matriz.component.ts
+++ src/app/components/matriz/matriz.component.ts
@@ -4,15 +4,30 @@ import { Component } from '@angular/core';
   selector: 'app-matriz',
   templateUrl: './matriz.component.html',
   styleUrls: ['./matriz.component.css'],
-  standalone: false,
 })
 export class MatrizComponent {
   jsonMatriz: string = '';
-  matrizRotada: string = '';
+  matrizRotada: number[][] = [];
 
+  // Función para mostrar la matriz
   rotarMatriz() {
-    if (this.jsonMatriz) {
-      this.matrizRotada = this.jsonMatriz;
+    try {
+
+      const matriz = JSON.parse(this.jsonMatriz);
+      console.log(matriz);
+
+      if (Array.isArray(matriz) && matriz.length == 2 && matriz.every((fila) => Array.isArray(fila) && fila.length == 2)) {
+        console.log('Matriz válida');
+        this.matrizRotada = matriz;
+      } else {
+        alert('Matriz inválida, se requiere una matriz 2x2');
+        this.matrizRotada = [];
+      }
+
+    } catch {
+      console.log('Error: Formato JSON inválido');
+      alert('Error: Formato JSON inválido, FORMATO VÁLIDO: [[X,X],[X,X]]');
+      this.matrizRotada = [];
     }
   }
 }
