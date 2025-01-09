959d4e8abc49927dc1ca41ae9108bcaec5307976
julio-1610 <jpaucarc@unsa.edu.pe>
2025-01-09 15:45:30 -0500

    Implementación base de HTML y botón muestra el string de jsonMatriz en un div

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { MatrizComponent } from './components/matriz/matriz.component';
@@ -10,7 +11,8 @@ import { MatrizComponent } from './components/matriz/matriz.component';
     MatrizComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/matriz/matriz.component.ts
+++ src/app/components/matriz/matriz.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-matriz',
   templateUrl: './matriz.component.html',
-  styleUrls: ['./matriz.component.css']
+  styleUrls: ['./matriz.component.css'],
+  standalone: false,
 })
 export class MatrizComponent {
+  jsonMatriz: string = '';
+  matrizRotada: string = '';
 
+  rotarMatriz() {
+    if (this.jsonMatriz) {
+      this.matrizRotada = this.jsonMatriz;
+    }
+  }
 }
