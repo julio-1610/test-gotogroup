07bac7b78ac5b672f1cc2e7c47330ae855631c99
julio-1610 <jpaucarc@unsa.edu.pe>
2025-01-09 15:19:45 -0500

    Creación de componente principal, rotador de matriz

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MatrizComponent } from './components/matriz/matriz.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MatrizComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/matriz/matriz.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-matriz',
+  templateUrl: './matriz.component.html',
+  styleUrls: ['./matriz.component.css']
+})
+export class MatrizComponent {
+
+}
