8f21949dba6d2aee4d5e350a88bf7b42169cd6bb
julio-1610 <jpaucarc@unsa.edu.pe>
2025-01-09 16:35:47 -0500

    Implementación de función rotarMatriz y cambio de errorJsonMatriz a booleano para que desaparezca una véz ingresado un valor correcto

--- src/app/components/matriz/matriz.component.ts
+++ src/app/components/matriz/matriz.component.ts
@@ -5,12 +5,14 @@ import { Component } from '@angular/core';
   templateUrl: './matriz.component.html',
   styleUrls: ['./matriz.component.css'],
 })
+
+// Componente para rotar una matriz NxN
 export class MatrizComponent {
   jsonMatriz: string = '';
   matrizRotada: number[][] = [];
-  errorJsonMatriz: string = "";
+  errorJsonMatriz: boolean = false;
 
-  // Función para mostrar la matriz
+  // Función para mostrar la matriz rotada
   botonRotar() {
     try {
 
@@ -18,21 +20,32 @@ export class MatrizComponent {
       console.log(matriz);
 
       if (Array.isArray(matriz) && matriz.length > 0 && matriz.every((fila) => Array.isArray(fila) && fila.length == matriz.length)) {
+        this.errorJsonMatriz = false;
         console.log('Matriz válida');
-        this.matrizRotada = matriz;
+        this.matrizRotada = this.rotarMatriz(matriz);
       } else {
         alert('Matriz inválida, se requiere una matriz NxN');
+        this.errorJsonMatriz = true;
         this.matrizRotada = [];
       }
 
     } catch {
       alert('Error: Formato JSON inválido');
-      this.errorJsonMatriz = "FORMATOS VÁLIDOS NxN: [[X,X],[X,X]] | [[X,X,X],[X,X,X],[X,X,X]] | ...";
+      this.errorJsonMatriz = true;
       this.matrizRotada = [];
     }
   }
 
+  // Función para rotar la matriz
   private rotarMatriz(matriz: number[][]): number[][] {
-    return matriz;
+    const N = matriz.length;
+    const matrizRotada = Array.from({ length: N }, () => Array.from({ length: N }, () => 0));
+
+    for (let i = 0; i < N; i++) {
+      for (let j = 0; j < N; j++) {
+        matrizRotada[N - j - 1][i] = matriz[i][j];
+      }
+    }
+    return matrizRotada;
   }
 }
