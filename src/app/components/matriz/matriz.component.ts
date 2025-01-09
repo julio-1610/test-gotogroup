a5d4ef8063140d7bcae31130162fb7e28ec5c5ae
julio-1610 <jpaucarc@unsa.edu.pe>
2025-01-09 16:22:23 -0500

    Creación de error que muestra al usuario los formatos válidos de JSON, y cambio de validaciones para NxN en lugar de 2x2

--- src/app/components/matriz/matriz.component.ts
+++ src/app/components/matriz/matriz.component.ts
@@ -8,26 +8,31 @@ import { Component } from '@angular/core';
 export class MatrizComponent {
   jsonMatriz: string = '';
   matrizRotada: number[][] = [];
+  errorJsonMatriz: string = "";
 
   // Función para mostrar la matriz
-  rotarMatriz() {
+  botonRotar() {
     try {
 
       const matriz = JSON.parse(this.jsonMatriz);
       console.log(matriz);
 
-      if (Array.isArray(matriz) && matriz.length == 2 && matriz.every((fila) => Array.isArray(fila) && fila.length == 2)) {
+      if (Array.isArray(matriz) && matriz.length > 0 && matriz.every((fila) => Array.isArray(fila) && fila.length == matriz.length)) {
         console.log('Matriz válida');
         this.matrizRotada = matriz;
       } else {
-        alert('Matriz inválida, se requiere una matriz 2x2');
+        alert('Matriz inválida, se requiere una matriz NxN');
         this.matrizRotada = [];
       }
 
     } catch {
-      console.log('Error: Formato JSON inválido');
-      alert('Error: Formato JSON inválido, FORMATO VÁLIDO: [[X,X],[X,X]]');
+      alert('Error: Formato JSON inválido');
+      this.errorJsonMatriz = "FORMATOS VÁLIDOS NxN: [[X,X],[X,X]] | [[X,X,X],[X,X,X],[X,X,X]] | ...";
       this.matrizRotada = [];
     }
+  }
+
+  private rotarMatriz(matriz: number[][]): number[][] {
+    return matriz;
   }
 }
